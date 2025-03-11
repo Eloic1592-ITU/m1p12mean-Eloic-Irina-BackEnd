57d7562ab90703bc7e27ea825205bc85d75bb854
Eloic Rakoto-Arisoa <eloicmax@gmail.com>
2025-03-11 16:42:19 +0300

    New changes
    Add file vercel.json
    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,34 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB connecté"))
-  .catch(err => console.log(err));
+// Désactive le buffering des commandes MongoDB
+mongoose.set('bufferCommands', false);
 
+// Connexion à MongoDB
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connecté");
+
+    // Démarrez le serveur Express une fois la connexion établie
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Serveur démarré sur le port ${process.env.PORT || 5000}`);
+    });
+  } catch (err) {
+    console.error("Erreur de connexion à MongoDB :", err);
+    process.exit(1); // Quitte l'application en cas d'erreur
+  }
+};
 // Routes
 app.use('/tests', require('./routes/testRoutes'));
 app.use('/products', require('./routes/productRoutes'));
